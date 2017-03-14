3a1483c9f28371c2d297723e7eddf0d58e70a128
Prasanna <prasannamishra50@gmail.com>
2017-03-14 19:33:52 +0545

    stopwatch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 import Clock from '../Clock/Clock';
+import Stopwatch from '../Stopwatch/Stopwatch';
 
 class App extends Component {
     constructor(props) {
         super(props);
         this.state = {
             deadline: 'December 25 2017',
-            newDeadline: ''
+            newDeadline: '',
+            timer: 200,
+            newTimer: 0
         };
     }
 
@@ -16,11 +19,12 @@ class App extends Component {
     }
 
 
+
     render() {
         return (
             <div className="App">
                 <div className="App-title"> Countdown to {this.state.deadline}</div>
-                <Clock
+                {/**<Clock
                     deadline={this.state.deadline}
                 />
                 <div>
@@ -30,6 +34,18 @@ class App extends Component {
                         onChange={event => this.setState({newDeadline: event.target.value})}
                     />
                     <button onClick={() => this.changeDeadline()}>Submit</button>
+                </div>*/}
+                <div className="Stopwatch-title"> StopWatch {this.state.timer}</div>
+                <Stopwatch
+                    timer={this.state.timer}
+                />
+                <div>
+                    <input
+                        type="text"
+                        placeholder="Enter time"
+                        onChange={event => this.setState({newTimer: event.target.value})}
+                    />
+                <button onClick={() => this.setState({timer: this.state.newTimer})}>Submit</button>
                 </div>
             </div>
         )
